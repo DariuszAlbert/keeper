2500ad54f54763434ff57dae502f23f5552c212d
Dariusz Golunski <darek.golunski@gmail.com>
2022-11-02 18:02:43 +0100

    First release

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return(
+        <div className="note">
+            <h1 >This is noto title</h1>
+            <p >this is note content</p>
+        </div>
+    );
+};
+
+export default Note;
